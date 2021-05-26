b21c85f22d1a876ccd502fd50644b48f761b841c
cuitaixiang <754060645@qq.com>
2021-05-26 23:23:57 +0800

    add comments

--- src/base58.cpp
+++ src/base58.cpp
@@ -17,7 +17,7 @@
 #include <assert.h>
 #include <string.h>
 
-
+// remove confusing characters
 /** All alphanumeric characters except for "0", "I", "O", and "l" */
 static const char* pszBase58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 
